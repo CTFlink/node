790a698357391d55a890a928bef3a9b69ff25d4d
Christian Flink <42993901+CTFlink@users.noreply.github.com>
2019-10-16 17:11:29 +0200

    http

--- myFirst.js
+++ myFirst.js
@@ -1,7 +1,11 @@
 var http = require('http');
+var fs = require('fs');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end(); //end the response
+    });
 }).listen(8080); //the server object listens on port 8080
